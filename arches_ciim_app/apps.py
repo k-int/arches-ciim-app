71f61e7fdfc18c835f01082519b742451ac7d190
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-05-18 15:34:14 +0100

    Upgrade 8.0

--- arches_ciim_app/apps.py
+++ arches_ciim_app/apps.py
@@ -1,13 +1,8 @@
 from django.apps import AppConfig
 from django.conf import settings
 
-from arches.settings_utils import generate_frontend_configuration
-
 
 class ArchesCiimAppConfig(AppConfig):
     name = "arches_ciim_app"
+    verbose_name = "Arches CIIM App"
     is_arches_application = True
-
-    def ready(self):
-        if settings.APP_NAME.lower() == self.name:
-            generate_frontend_configuration()
