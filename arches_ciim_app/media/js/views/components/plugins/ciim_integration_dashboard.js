48a68cde839a8b9e928d618871731178102699a4
Samuel Scandrett <samuel.scandrett@k-int.co.uk>
2026-05-18 15:40:14 +0100

    nit

--- arches_ciim_app/media/js/views/components/plugins/ciim_integration_dashboard.js
+++ arches_ciim_app/media/js/views/components/plugins/ciim_integration_dashboard.js
@@ -1,5 +1,5 @@
-import ko from knockout;
-import arches from arches;
+import ko from 'knockout';
+import arches from 'arches';
 import CIIMIntegrationDashboardTemplate from 'templates/views/components/plugins/ciim_integration_dashboard.htm';
 
 const CIIMIntegrationDashboardViewModel = function() {
